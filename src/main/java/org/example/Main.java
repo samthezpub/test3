1e4d692b0533ca2a630713bcec1e7f959bc351b4
dxrkfuture <dxrkfuture@gmail.com>
2023-10-07 15:27:48 +0300

    add sex to main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
 
 public class Main {
+    public static void sex(){
+        System.out.println("Ban");
+    }
     public static void printHello(){
         System.out.println("Hello world!");
     }
     public static void main(String[] args) {
         printHello();
+        sex();
     }
 }
